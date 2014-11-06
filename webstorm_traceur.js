fc41bc1feb3344042125a0686567847f00d55f3f
Vojta Jina <vojta.jina@gmail.com>
2014-11-06 11:58:12 -0800

    WebStorm: generic sourceRoot for source maps

--- webstorm_traceur.js
+++ webstorm_traceur.js
@@ -1,9 +1,13 @@
 var inputFilename = process.argv[2];
 var outputFilename = process.argv[3] || inputFilename.replace(/\.ats$/, '.js');
 
+var path = require('path');
+var sourceRoot = path.relative(path.dirname(outputFilename), '');
+
 var TraceurNodeCompiler = require('traceur/src/node/NodeCompiler').NodeCompiler;
 var options = require('./config.json').traceur;
-var compiler = new TraceurNodeCompiler(options, '../../');
+var compiler = new TraceurNodeCompiler(options);
+
 
 // Increment the lock to delay Karma unit tests until all watchers are finished.
 var LOCK_FILE = '.atscriptwatcher';
@@ -17,17 +21,21 @@ if (fs.existsSync(LOCK_FILE)) {
   fs.writeFileSync(LOCK_FILE, '1');
 }
 
-// TODO(vojta): fix this in Traceur instead
+
+// TODO(vojta): Fix this in Traceur instead.
 // Traceur generates source map file in the CWD.
-// This hacks Traceur to generate source map file in the output file directory.
+// This hacks Traceur to generate source map file in the output file directory, respecting the `sourceRoot` option.
 var writeFile = require('traceur/src/node/file-util').writeFile;
 compiler.writeTreeToFile = function(tree, filename) {
   filename = this.normalize(filename);
-  var compiledCode = this.write(tree, filename, this.sourceRoot_);
+  // This is changed.
+  // Pass sourceRoot to `this.write`. Traceur does not pass it through.
+  var compiledCode = this.write(tree, filename, sourceRoot);
   if (this.options_.sourceMaps === 'file') {
     var sourcemap = this.getSourceMap();
     if (sourcemap) {
-      // This is the changed line
+      // This is changed.
+      // Generate the source map in the same folder as the output JS file.
       writeFile(filename.replace(/\.js$/, '.map'), sourcemap);
     }
   }
